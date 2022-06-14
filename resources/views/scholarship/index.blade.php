98a9e02a2c0e91c025d83ef22697f72c45c32044
Just an ordinary girl <70735803+faqirilmu31@users.noreply.github.com>
2022-06-14 15:52:10 +0700

    change criteria input to array

--- resources/views/scholarship/index.blade.php
+++ resources/views/scholarship/index.blade.php
@@ -4,43 +4,44 @@
 
 @section('content')
 <form action={{ route('scholarship.criteria') }} method="post">
+    @csrf
     <div class="mb-3">
-        <input type="checkbox" name="criteria" value="poin_spkk"> Poin SPKK <br />
+        <input type="checkbox" name="criteria[]" value="poin_spkk"> Poin SPKK <br />
         <ul style="list-style-type:none;">
             <li> <input type="radio" name="isPoinSPKKCost" value="false"> Benefit </li>
             <li> <input type="radio" name="isPoinSPKKCost" value="true"> Cost </li>
         </ul>
     </div>
     <div class="mb-3">
-        <input type="checkbox" name="criteria" value="ipk"> IPK <br />
+        <input type="checkbox" name="criteria[]" value="ipk"> IPK <br />
         <ul style="list-style-type:none;">
             <li> <input type="radio" name="isIPKCost" value="false"> Benefit </li>
             <li> <input type="radio" name="isIPKCost" value="true"> Cost </li>
         </ul>
     </div>
     <div class="mb-3">
-        <input type="checkbox" name="criteria" value="penghasilan"> Penghasilan <br />
+        <input type="checkbox" name="criteria[]" value="penghasilan"> Penghasilan <br />
         <ul style="list-style-type:none;">
             <li> <input type="radio" name="isPenghasilanCost" value="false"> Benefit </li>
             <li> <input type="radio" name="isPenghasilanCost" value="true"> Cost </li>
         </ul>
     </div>
     <div class="mb-3">
-        <input type="checkbox" name="criteria" value="pengeluaran"> Pengeluaran <br />
+        <input type="checkbox" name="criteria[]" value="pengeluaran"> Pengeluaran <br />
         <ul style="list-style-type:none;">
             <li> <input type="radio" name="isPengeluaranCost" value="false"> Benefit </li>
             <li> <input type="radio" name="isPengeluaranCost" value="true"> Cost </li>
         </ul>
     </div>
     <div class="mb-3">
-        <input type="checkbox" name="criteria" value="selisih"> Penghasilan-Pengeluaran <br />
+        <input type="checkbox" name="criteria[]" value="selisih"> Penghasilan-Pengeluaran <br />
         <ul style="list-style-type:none;">
             <li> <input type="radio" name="isSelisihCost" value="false"> Benefit </li>
             <li> <input type="radio" name="isSelisihCost" value="true"> Cost </li>
         </ul>
     </div>
     <div class="mb-3">
-        <input type="checkbox" name="criteria" value="tanggungan"> Tanggungan <br />
+        <input type="checkbox" name="criteria[]" value="tanggungan"> Tanggungan <br />
         <ul style="list-style-type:none;">
             <li> <input type="radio" name="isTanggunganCost" value="false"> Benefit </li>
             <li> <input type="radio" name="isTanggunganCost" value="true"> Cost </li>
