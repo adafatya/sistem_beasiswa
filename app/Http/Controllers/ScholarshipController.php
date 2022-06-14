91c3ca73053d766907476a5bde750e604c0813d4
Just an ordinary girl <70735803+faqirilmu31@users.noreply.github.com>
2022-06-14 15:52:22 +0700

    create matrix

--- app/Http/Controllers/ScholarshipController.php
+++ app/Http/Controllers/ScholarshipController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ScholarshipController extends Controller
 {
@@ -11,9 +12,30 @@ class ScholarshipController extends Controller
         return view('scholarship.index');
     }
 
-    public function criteria()
+    public function criteria(Request $request)
     {
-        return view('scholarship.show');
-    }
+        $query = 'nama, ';
 
+        $criteria = $request->input('criteria');
+        for ($i = 0; $i < count($criteria); $i++) {
+            if ($criteria[$i] == 'selisih') {
+                $query .= '(penghasilan-pengeluaran) as selisih, ';
+            } else {
+                $query .= $criteria[$i] . ', ';
+            }
+        }
+        $query = substr_replace($query, '', strlen($query) - 2, 2);
+
+        $data = DB::table('students')
+            ->selectRaw($query)
+            ->get();
+
+        $matrix = [];
+
+        foreach ($data as $student) {
+            array_push($matrix, (array) $student);
+        }
+
+        dd($matrix);
+    }
 }
