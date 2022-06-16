fe87b79f8fd59f4cd27b1af280b9ca6a60c5a480
Just an ordinary girl <70735803+faqirilmu31@users.noreply.github.com>
2022-06-16 09:10:56 +0700

    add weight

--- resources/views/scholarship/index.blade.php
+++ resources/views/scholarship/index.blade.php
@@ -5,47 +5,57 @@
 @section('content')
 <form action={{ route('scholarship.criteria') }} method="post">
     @csrf
-    <div class="mb-3">
-        <input type="checkbox" name="criteria[]" value="poin_spkk"> Poin SPKK <br />
-        <ul style="list-style-type:none;">
-            <li> <input type="radio" name="isPoinSPKKCost" value="false"> Benefit </li>
-            <li> <input type="radio" name="isPoinSPKKCost" value="true"> Cost </li>
-        </ul>
+    <div class="col">
+        <div class="mb-3">
+            <input type="checkbox" name="criteria[]" value="poin_spkk"> Poin SPKK <br />
+            <ul style="list-style-type:none;">
+                <li> <input type="radio" name="isPoinSPKKCost" value="false"> Benefit </li>
+                <li> <input type="radio" name="isPoinSPKKCost" value="true"> Cost </li>
+                <li> Weight <input type="number" name="poin_spkk_weight" step="0.01"> </li>
+            </ul>
+        </div>
+        <div class="mb-3">
+            <input type="checkbox" name="criteria[]" value="ipk"> IPK <br />
+            <ul style="list-style-type:none;">
+                <li> <input type="radio" name="isIPKCost" value="false"> Benefit </li>
+                <li> <input type="radio" name="isIPKCost" value="true"> Cost </li>
+                <li> Weight <input type="number" name="ipk_weight" step="0.01"> </li>
+            </ul>
+        </div>
+        <div class="mb-3">
+            <input type="checkbox" name="criteria[]" value="tanggungan"> Tanggungan <br />
+            <ul style="list-style-type:none;">
+                <li> <input type="radio" name="isTanggunganCost" value="false"> Benefit </li>
+                <li> <input type="radio" name="isTanggunganCost" value="true"> Cost </li>
+                <li> Weight <input type="number" name="tanggungan_weight" step="0.01"> </li>
+            </ul>
+        </div>
     </div>
-    <div class="mb-3">
-        <input type="checkbox" name="criteria[]" value="ipk"> IPK <br />
-        <ul style="list-style-type:none;">
-            <li> <input type="radio" name="isIPKCost" value="false"> Benefit </li>
-            <li> <input type="radio" name="isIPKCost" value="true"> Cost </li>
-        </ul>
-    </div>
-    <div class="mb-3">
-        <input type="checkbox" name="criteria[]" value="penghasilan"> Penghasilan <br />
-        <ul style="list-style-type:none;">
-            <li> <input type="radio" name="isPenghasilanCost" value="false"> Benefit </li>
-            <li> <input type="radio" name="isPenghasilanCost" value="true"> Cost </li>
-        </ul>
-    </div>
-    <div class="mb-3">
-        <input type="checkbox" name="criteria[]" value="pengeluaran"> Pengeluaran <br />
-        <ul style="list-style-type:none;">
-            <li> <input type="radio" name="isPengeluaranCost" value="false"> Benefit </li>
-            <li> <input type="radio" name="isPengeluaranCost" value="true"> Cost </li>
-        </ul>
-    </div>
-    <div class="mb-3">
-        <input type="checkbox" name="criteria[]" value="selisih"> Penghasilan-Pengeluaran <br />
-        <ul style="list-style-type:none;">
-            <li> <input type="radio" name="isSelisihCost" value="false"> Benefit </li>
-            <li> <input type="radio" name="isSelisihCost" value="true"> Cost </li>
-        </ul>
-    </div>
-    <div class="mb-3">
-        <input type="checkbox" name="criteria[]" value="tanggungan"> Tanggungan <br />
-        <ul style="list-style-type:none;">
-            <li> <input type="radio" name="isTanggunganCost" value="false"> Benefit </li>
-            <li> <input type="radio" name="isTanggunganCost" value="true"> Cost </li>
-        </ul>
+    <div class="col">
+        <div class="mb-3">
+            <input type="checkbox" name="criteria[]" value="penghasilan"> Penghasilan <br />
+            <ul style="list-style-type:none;">
+                <li> <input type="radio" name="isPenghasilanCost" value="false"> Benefit </li>
+                <li> <input type="radio" name="isPenghasilanCost" value="true"> Cost </li>
+                <li> Weight <input type="number" name="penghasilan_weight" step="0.01"> </li>
+            </ul>
+        </div>
+        <div class="mb-3">
+            <input type="checkbox" name="criteria[]" value="pengeluaran"> Pengeluaran <br />
+            <ul style="list-style-type:none;">
+                <li> <input type="radio" name="isPengeluaranCost" value="false"> Benefit </li>
+                <li> <input type="radio" name="isPengeluaranCost" value="true"> Cost </li>
+                <li> Weight <input type="number" name="pengeluaran_weight" step="0.01"> </li>
+            </ul>
+        </div>
+        <div class="mb-3">
+            <input type="checkbox" name="criteria[]" value="selisih"> Penghasilan-Pengeluaran <br />
+            <ul style="list-style-type:none;">
+                <li> <input type="radio" name="isSelisihCost" value="false"> Benefit </li>
+                <li> <input type="radio" name="isSelisihCost" value="true"> Cost </li>
+                <li> Weight <input type="number" name="selisih_weight" step="0.01"> </li>
+            </ul>
+        </div>
     </div>
     <button type="submit">Lanjut</button>
 </form>
